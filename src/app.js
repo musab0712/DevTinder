93c2ad2f963f3b065687030c716c52043aa10974
Musab Hassan <musabhassan1999@gmail.com>
2024-12-01 18:44:54 +0530

    explore app.use()

--- src/app.js
+++ src/app.js
@@ -2,14 +2,20 @@ const expess = require("express");
 
 const app = expess();
 
-app.get("/user", (req, res) => {
-    res.send("heelo from server...")
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("user 1");
+    //res.send("res 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("user 2");
+    res.send("res 2");
+    //next();
+  }
+);
+
+app.listen(3000, () => {
+  console.log("server started at 3000 port....");
 });
-
-app.use("/", (req, res) => {
-    res.send("heelo from server...")
-})
-
-app.listen(3000, ()=>{
-    console.log("server started at 3000 port....")
-})
