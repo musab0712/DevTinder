fe862e6f51de44b5a889f43045774cda9edd423e
Musab Hassan <musabhassan1999@gmail.com>
2024-12-08 13:29:23 +0530

    create user model and post data api

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 const expess = require("express");
 const connectDB = require("./config/db");
-
+const User = require("./models/user");
 const app = expess();
+
+app.post("/getData", async (req, res) => {
+  const user = new User({
+    firstName: "Musab",
+    lastName: "Hassan",
+    emailId: "musab@gmail.com",
+    password: "musab@123",
+  });
+
+  await user.save();
+  res.send("Data Sava Successful");
+});
 
 connectDB()
   .then(() => {
     console.log("DataBase Connected...");
+    app.listen(3000, () => {
+      console.log("server started at 3000 port....");
+    });
   })
   .catch((err) => {
     console.log("DB Not Connected " + err);
   });
-
-app.listen(3000, () => {
-  console.log("server started at 3000 port....");
-});
